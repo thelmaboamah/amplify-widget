914623ff82458fab224ac9b37f4153a71b422fd2
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-04-01 22:44:16 -0700

    accordion working

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,16 +13,41 @@ $(document).ready(function(){
 	var dbRef = firebase.database().ref().child('actions');
 
 	dbRef.on('value', snap => {
-
+		$("#accordion").empty();
 		var snapshot = snap.val();
+		var currentAction;
+		var id = 0;
+		console.log(currentAction)
 
 		for (var action in snapshot) {
-		  var currentAction;
+		  
 		  
 		  if (snapshot.hasOwnProperty(action)) {
 		     currentAction = snapshot[action]
+		     console.log(currentAction)
+		    
+		    var actionDiv = `
+		    <div class="card">
+		      <div class="card-header" role="tab" id="heading${++id}">
+	          <div data-toggle="collapse" data-parent="#accordion" href="#collapse${id}" aria-expanded="false" aria-controls="collapse${id}">
+	            ${currentAction.verbPreTitle}
+	          </div>
+		      </div>
 
-		    $("#actions").append(`<p>${currentAction.bodyMarkdown}</p>`)
+		      <div id="collapse${id}" class="collapse" role="tabpanel" aria-labelledby="heading${id}">
+		        <div class="card-block">
+		          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		        </div>
+		      </div>
+		    </div>
+
+		    `
+
+
+
+
+
+		    $("#accordion").append(actionDiv);
 		    
 
 
@@ -30,6 +55,7 @@ $(document).ready(function(){
 
 		  }
 		}
+
 		
 	});
 	
