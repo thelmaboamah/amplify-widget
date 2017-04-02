ac0d0019ea05a83d959ca362626759392a20ccb4
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-04-02 09:21:10 -0700

    almost have flash messages working

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,14 +17,15 @@ $(document).ready(function(){
 		var snapshot = snap.val();
 		var currentAction;
 		var id = 0;
-		console.log(currentAction)
+		var message = ["Way to go!", "Nice work!", "Good job!"];
+		var randMessage;
+		
 
 		for (var action in snapshot) {
 		  
 		  
 		  if (snapshot.hasOwnProperty(action)) {
 		     currentAction = snapshot[action]
-		     console.log(currentAction)
 		    
 		    var actionDiv = `
 		    <div class="card">
@@ -55,6 +56,7 @@ $(document).ready(function(){
 		         <p class="script">
 							${currentAction.script}
 		         </p>
+		         <div class="flash-message">${randMessage}</div>
 		         <div class="action-buttons">
 							<button class="btn">Spoke</button>
 							<button class="btn">Left Voicemail</button>
@@ -69,6 +71,17 @@ $(document).ready(function(){
 
 		  }
 		}	
+	
+		$("#accordion").on("click", ".action-buttons .btn", function(e) {
+			randMessage = message[Math.floor(Math.random() * message.length)]
+			console.log(randMessage)
+			var fMessage = $(this).parent().prev(".flash-message")
+			fMessage.css("display", "show").delay(800).hide();
+		})
+
+
 	});
-		
+
+
+	
 });
